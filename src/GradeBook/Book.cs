68b86ae32cc432b0f5c1529f2e78ef874b638973
Erica <ericaperry@quickenloans.com>
2020-10-08 17:44:22 -0400

    add method to get average

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,10 +11,17 @@ namespace GradeBook
         public void AddGrade(double newGrade)
         {
             grades.Add(newGrade);
+        }
+        public void GetAverage()
+        {
+            var average = 0.0;
             foreach (var grade in grades)
             {
+                average += grade;
                 System.Console.WriteLine(grade);
             }
+            average = average/grades.Count;
+            System.Console.WriteLine($"The average grade is {average:N1}.");
         }
         List<double> grades;
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,6 +9,10 @@ namespace GradeBook
         {
             var book = new Book();
             book.AddGrade(99.1);
+            book.AddGrade(56.9);
+            book.AddGrade(88);
+
+            book.GetAverage();
         }
     }
 }

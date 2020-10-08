16564467a9b0b821b8c0a48c6848965405e14bf5
Erica <ericaperry@quickenloans.com>
2020-10-08 16:45:50 -0400

    use lists instead of arrays

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double>() { 20.03, 20.03, 20.03 };
+            grades.Add(88.2);
+            var result = 0.0;
+
+            foreach(double number in grades) 
+            {
+                result += number;
+            }
+            System.Console.WriteLine(result/grades.Count);
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}!");

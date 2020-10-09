d3d37636b08f1d88f0928c0fce8aa6003ae54c4e
Erica <ericaperry@quickenloans.com>
2020-10-09 09:38:35 -0400

    add tests to for book

---
+++ test/Gradebook.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(99.1);
+            book.AddGrade(56.9);
+            book.AddGrade(88.3);
+            book.AddGrade(65.7);
+
+            // act
+            var average = book.GetAverage();
+            var low = book.GetLowestGrade();
+            var high = book.GetHighestGrade();
+
+            // assert
+            Assert.Equal("77.50", average);
+            Assert.Equal("56.90", low);
+            Assert.Equal("99.10", high);
+        }
+    }
+}
